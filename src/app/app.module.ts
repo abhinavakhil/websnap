6c3ed61a0a70e9bc34bfd9da627404cbabe6bf1f
im-abhinav <meetspacegravity@gmail.com>
2021-09-25 12:30:52 +0530

    web-snap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LovecalcComponent } from './lovecalc/lovecalc.component';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { DataService } from './service/data.service';
-import { PictureComponent } from './picture/picture.component';
-import { AngularFireModule } from 'angularfire2';
-import { environment } from '../environments/environment';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { DataService } from "./service/data.service";
+import { PictureComponent } from "./picture/picture.component";
+import { AngularFireModule } from "angularfire2";
+import { environment } from "../environments/environment";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import {
+  MatToolbarModule,
+  MatIconModule,
+  MatListModule,
+  MatButtonModule,
+} from "@angular/material";
+import { ToastrModule } from "ngx-toastr";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LovecalcComponent,
-    PictureComponent
-  ],
+  declarations: [AppComponent, PictureComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    AngularFireModule.initializeApp(environment.firebase)
+    AngularFireModule.initializeApp(environment.firebase),
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    ToastrModule.forRoot({
+      timeOut: 10000,
+      positionClass: "toast-bottom-right",
+      preventDuplicates: true,
+    }),
   ],
   providers: [DataService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/picture/picture.component.ts
+++ src/app/picture/picture.component.ts
@@ -1,73 +1,76 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { PathLocationStrategy } from '@angular/common';
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import { ToastrService } from "ngx-toastr";
 
 @Component({
-  selector: 'app-picture',
-  templateUrl: './picture.component.html',
-  styleUrls: ['./picture.component.css']
+  selector: "app-picture",
+  templateUrl: "./picture.component.html",
+  styleUrls: ["./picture.component.css"],
 })
 export class PictureComponent implements OnInit {
-   width=700;
-   height=0;
-   filter='none';
-   streaming=false;
-   video: HTMLVideoElement;
-///taking photo
-   @ViewChild("canvas")
-    public canvas: ElementRef;
-    captures:Array<any>;
-//filter
-  @ViewChild("photofilter") photoFilter :ElementRef;
-  @ViewChild('clearButton') clearButton :ElementRef;
-  @ViewChild('a') alpha:ElementRef;
+  @ViewChild("canvas") public canvas: ElementRef;
+  @ViewChild("photofilter") photoFilter: ElementRef;
+  @ViewChild("clearButton") clearButton: ElementRef;
+  @ViewChild("a") alpha: ElementRef;
 
-//
-   constraints = { audio: false, video:true };
-  constructor() {
+  width: number = 700;
+  height: number = 0;
+  filter: string = "none";
+  streaming: boolean = false;
+  video: HTMLVideoElement;
+  constraints = { audio: false, video: true };
+  hasMedia = { hasVideo: false, hasAudio: false };
+  captures: Array<{ img: any; filter: string }>;
+
+  constructor(private toastr: ToastrService) {
     this.captures = [];
-   }
-
-  ngOnInit() {
-
-    this.video = document.querySelector('video');
-    navigator.mediaDevices.getUserMedia(this.constraints).then(
-      stream => {
-        this.video.srcObject = stream;
-        this.video.play();
-      },
-      error => {
-        console.log('Error: ' + error);
-      });
-///filtering
-this.photoFilter.nativeElement.addEventListener('change',(e)=>{
-  this.filter=e.target.value;
-  
-  this.video.style.filter = this.filter;
-  e.preventDefault();
-});
-//clear event
- this.clearButton.nativeElement.addEventListener('click',(e)=>{
-   //this.captures.innerHTML="";
-   //set filter none;
-   this.filter='none';
-   //set video filter
-   this.video.style.filter=this.filter;
-   //reset select list
-    // y.selectedIndex = 0;
- })
-
-
   }
 
+  ngOnInit() {
+    this.video = document.querySelector("video");
+    navigator.mediaDevices.getUserMedia(this.constraints).then(
+      (stream) => {
+        this.video.srcObject = stream;
+        this.video.play();
+        this.checkStream(stream);
+      },
+      (error) => {
+        this.toastr.error("An error occured, please refresh");
+        console.log("Error: " + error);
+      }
+    );
+  }
 
-   takepicture() {
-    let context = this.canvas.nativeElement.getContext('2d')
-    .drawImage(this.video, 0, 0, 700, 480);
+  filterImage(value) {
+    this.filter = value;
+    this.video.style.filter = this.filter;
+  }
 
-    this.captures.push(this.canvas.nativeElement.toDataURL("image/png"));
-    (document.querySelector('.app') as HTMLElement).style.filter = this.filter;
+  clear() {
+    this.captures = [];
+    this.filter = "none";
+    this.video.style.filter = this.filter;
+  }
+
+  takepicture() {
+    if (this.hasMedia.hasVideo) {
+      const context = this.canvas.nativeElement.getContext("2d");
+      context.drawImage(this.video, 0, 0, 700, 480);
+      const imgUrl = this.canvas.nativeElement.toDataURL("image/png");
+
+      let payload = { img: imgUrl, filter: this.filter };
+      this.captures.push(payload);
+    } else {
+      this.toastr.error("initializing video please wait");
     }
+  }
 
+  checkStream(stream) {
+    if (stream.active) {
+      this.hasMedia.hasVideo = true;
+      if (this.hasMedia.hasVideo) {
+        this.toastr.success("Video initialized, take awesome pic: )");
+      }
+    }
+    return this.hasMedia;
+  }
 }
-
-
